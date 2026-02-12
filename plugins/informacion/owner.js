94fa771de0e5a70b48fb637836674351df62da40
JUAN MD <jb2009202112@gmail.com>
2026-02-12 18:56:53 -0500

    nuevo

---
+++ plugins/informacion/owner.js
@@ -0,0 +1,65 @@
+import PhoneNumber from 'awesome-phonenumber';
+
+async function handler(m, { conn }) {
+  m.react('👑');
+  const numCreador = '50377798434';
+  const ownerJid = numCreador + '@s.whatsapp.net';
+
+    const res = await fetch('https://files.catbox.moe/cduhlw.jpg');
+const thumb2 = Buffer.from(await res.arrayBuffer());
+
+  const fkontak = {
+    key: {
+      participants: "0@s.whatsapp.net",
+      remoteJid: "status@broadcast",
+      fromMe: false,
+      id: "Halo"
+    },
+    message: {
+      locationMessage: {
+        name: '𝗖𝗥𝗘𝗔𝗗𝗢𝗥 👑',
+        jpegThumbnail: thumb2
+      }
+    },
+    participant: "0@s.whatsapp.net"
+  };
+
+  const name = await conn.getName(ownerJid) || 'Deylin';
+  const about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || ' Servicios técnicos de bots para WhatsApp';
+  const empresa = ' Servicios Tecnológicos';
+
+
+  const vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:;${name};;;
+FN:${name}
+ORG:${empresa};
+TITLE:CEO & Fundador
+TEL;waid=${numCreador}:${new PhoneNumber('+' + numCreador).getNumber('international')}
+EMAIL:correo@empresa.com
+URL:https://www.tuempresa.com
+NOTE:${about}
+ADR:;;Dirección de tu empresa;;;;
+X-ABADR:ES
+X-ABLabel:Dirección Web
+X-ABLabel:Correo Electrónico
+X-ABLabel:Teléfono de contacto
+X-WA-BIZ-NAME:${name}
+X-WA-BIZ-DESCRIPTION:${about}
+END:VCARD
+  `.trim();
+
+
+  await conn.sendMessage(
+    m.chat,
+    { contacts: { displayName: name, contacts: [{ vcard }] } },
+    { quoted: fkontak }
+  );
+}
+
+handler.help = ['owner'];
+handler.tags = ['main'];
+handler.command = ['owner', 'creator', 'creador', 'reportar'];
+
+export default handler;
